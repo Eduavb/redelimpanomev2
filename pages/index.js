c198b41bb6b5dddfb307dceb1aad97464f1c0737
Eduavb <eduardoavbarros@gmail.com>
2025-05-30 19:39:12 -0300

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", padding: "80px" }}>
+      <h1>Bem-vindo à Rede Limpa Nome</h1>
+      <p>Seu sistema está online e funcionando corretamente.</p>
+    </div>
+  );
+}
